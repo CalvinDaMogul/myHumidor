3fc94984130ce4433eaee42cc66941ccd97950bb
CalvinDaMogul <cal.j.foster@gmail.com>
2019-07-21 02:46:04 -0500

    able to read cards from firebase

--- src/Helpers/data/cigars.js
+++ src/Helpers/data/cigars.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import firebaseConfig from '../apiKeys.json';
 
-const baseUrl = firebaseConfig.firebaseKeys.databaseUrl;
+const baseUrl = firebaseConfig.firebaseKeys.databaseURL;
 const getMyCigars = uid => new Promise((resolve, reject) => {
   axios
     .get(`${baseUrl}/cigars.json?orderBy="uid"&equalTo="${uid}"`)
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -15,8 +15,8 @@ class Home extends React.Component {
     getCigars = () => {
       const { uid } = firebase.auth().currentUser;
       cigarData.getMyCigars(uid)
-        .then(scats => this.setState({ scats }))
-        .catch(err => console.error('could not get any cigars', err));
+        .then(cigars => this.setState({ cigars }))
+        .catch(err => console.error('could not get any cigars fam', err));
     }
 
     componentDidMount() {
@@ -24,7 +24,7 @@ class Home extends React.Component {
     }
 
     render() {
-      const makeCigarCards = this.state.cigsrs.map(cigar => (
+      const makeCigarCards = this.state.cigars.map(cigar => (
             <CigarCard
               key={cigar.id}
               cigar={cigar}
