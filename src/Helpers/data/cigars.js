533b436b7481a354d4250e69a570bde3e5ad2596
CalvinDaMogul <cal.j.foster@gmail.com>
2019-07-20 22:23:41 -0500

    added pictures

---
+++ src/Helpers/data/cigars.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import firebaseConfig from '../apiKeys.json';
+
+const baseUrl = firebaseConfig.firebaseKeys.databaseUrl;
+const getMyCigars = uid => new Promise((resolve, reject) => {
+  axios
+    .get(`${baseUrl}/cigars.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((res) => {
+      const cigars = [];
+      if (res.data !== null) {
+        Object.keys(res.data).forEach((fbKey) => {
+          res.data[fbKey].id = fbKey;
+          cigars.push(res.data[fbKey]);
+        });
+      }
+      resolve(cigars);
+    })
+    .catch(err => reject(err));
+});
+
+const getSingleCigar = cigarId => axios.get(`${baseUrl}/cigars/${cigarId}.json`);
+
+export default { getSingleCigar, getMyCigars };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,46 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/auth';
+
+import cigarData from '../../Helpers/data/cigars';
+import CigarCard from '../CigarCard/CigarCard';
 
 import './Home.scss';
 
 class Home extends React.Component {
-  render() {
-    return (
+    state = {
+      cigars: [],
+    }
+
+    getCigars = () => {
+      const { uid } = firebase.auth().currentUser;
+      cigarData.getMyCigars(uid)
+        .then(scats => this.setState({ scats }))
+        .catch(err => console.error('could not get any cigars', err));
+    }
+
+    componentDidMount() {
+      this.getCigars();
+    }
+
+    render() {
+      const makeCigarCards = this.state.cigsrs.map(cigar => (
+            <CigarCard
+              key={cigar.id}
+              cigar={cigar}
+
+            />
+      ));
+      return (
       <div className="Home col">
         <h1>Cigarficionado</h1>
         <div className="d-flex">
+            {makeCigarCards}
         </div>
 
       </div>
-    );
-  }
+      );
+    }
 }
 
 export default Home;
